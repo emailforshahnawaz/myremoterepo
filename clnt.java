69f2b5b4016d656b337b3fd12888b4a9f2070534
wmailforshahnawaz <emailforshahnawaz@gmail.com>
2019-03-30 02:53:29 +0530

    initial commit

---
+++ clnt.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+class clnt{
+public static void main(String arg[])throws Exception{
+Socket s=new Socket("localhost",9999);
+BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+int random1=Integer.parseInt(br.readLine());
+System.out.println("server sent you "+random1);
+int random=random1+1;
+
+// Decode
+         byte[] random2 = Base64.getDecoder().decode(random);
+		
+        // System.out.println("Original String: " + new String(base64decodedBytes, "utf-8"));
+OutputStreamWriter os;
+PrintWriter out;
+
+os=new OutputStreamWriter(s.getOutputStream());
+out=new PrintWriter(os);
+os.flush();
+out.println(random1);
+os.flush();
+
+Scanner sc=new Scanner(System.in);
+//BufferedReader br;
+
+while(true){
+String str=sc.nextLine();
+
+out.println(str);
+os.flush();
+
+String str1=br.readLine();
+System.out.println(str1);
+}
+}
+}
---
+++ srvr.java
@@ -0,0 +1,57 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class srvr{
+public static void main(String arg[])throws Exception{
+ServerSocket ss=new ServerSocket(9999);
+OutputStreamWriter os;
+PrintWriter out;
+BufferedReader br;
+Scanner sc;
+Socket s=ss.accept();
+System.out.println("client connnected");
+Random rand = new Random(); 
+  
+        // Generate random integers in range 0 to 999 
+        int rand_int1 = rand.nextInt(1000); 
+      int random_copy = rand_int1;
+
+ // Encode using basic encoder
+         String base64encodedString = Base64.getEncoder().encodeToString(
+           rand_int1.getBytes("utf-8"));
+         //System.out.println("Base64 Encoded String (Basic) :" + base64encodedString);
+os=new OutputStreamWriter(s.getOutputStream());
+out=new PrintWriter(os);
+out.println(rand_int1);
+os.flush();
+br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+
+//os.flush();
+int max=50,min=1;
+
+
+int rc=Integer.parseInt(br.readLine());
+rc=rc-1;
+if(rc==random_copy+1){
+
+System.out.println("Authentication succesfull");
+}
+else System.exit(0);
+while(true){
+
+
+
+String str1=br.readLine();
+System.out.println(str1);
+
+String str=sc.nextLine();
+os=new OutputStreamWriter(s.getOutputStream());
+out=new PrintWriter(os);
+os.flush();
+out.println(str);
+os.flush();
+}
+}
+}
